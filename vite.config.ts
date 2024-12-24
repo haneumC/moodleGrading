a9ea6805365d56db42658993a43ef18efd1737d7
Haneum Cha <che23248927@gmail.com>
2024-12-23 23:40:52 -0500

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/MoodleGradingApp2.0/',
+  base: 'https://haneumc.github.io/moodleGrading2.0.1/',
 })
 
